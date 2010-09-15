6520b68328caaa65dcc2124b8236962fd79bd2bf
eldiener <eldiener@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-09-15 19:27:47 +0000

    Original test files
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/variadic_macro_data@65428 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ libs/variadic_macro_data/test/TestData.cpp
@@ -0,0 +1,33 @@
+#include <boost/variadic_macro_data/VariadicMacroData.hpp>
+#include <boost/detail/lightweight_test.hpp>
+#include <vector>
+#include <boost/type_traits/is_same.hpp>
+
+int main()
+  {
+  
+#if !defined(BOOST_NO_VARIADIC_MACROS)
+
+  int aSize(VMD_DATA_SIZE(23,ll,678,456));
+  
+  BOOST_TEST_EQ(aSize,4);
+  
+  int anElement(VMD_DATA_ELEMENT(2,23,ll,678,456));
+  
+  BOOST_TEST_EQ(anElement,678);
+  
+  #define VMD_TEST_EXPAND(...) int,__VA_ARGS__
+  
+  int numberOfTypes(VMD_DATA_SIZE(VMD_TEST_EXPAND(unsigned,long,short,unsigned long)));
+  
+  BOOST_TEST_EQ(numberOfTypes,5);
+  
+  typedef std::vector<VMD_DATA_ELEMENT(0,VMD_TEST_EXPAND(unsigned,long,short,unsigned long))>::value_type vtype;
+  
+  BOOST_TEST((boost::is_same<vtype,int>::value));
+  
+#endif
+
+  return boost::report_errors();
+  
+  }
