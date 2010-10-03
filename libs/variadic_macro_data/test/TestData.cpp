d46a5b416da33b1cb0cd36afd8ed79e64b72d144
eldiener <eldiener@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-10-03 23:19:20 +0000

    Updated VMD_DATA_ELEM macro to work with VC++ and expanded data test.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/variadic_macro_data@65743 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/variadic_macro_data/test/TestData.cpp
+++ libs/variadic_macro_data/test/TestData.cpp
@@ -2,15 +2,32 @@
 #include <boost/detail/lightweight_test.hpp>
 #include <vector>
 #include <boost/type_traits/is_same.hpp>
+#include <boost/preprocessor/arithmetic/inc.hpp>
 
 int main()
   {
   
 #if !defined(BOOST_NO_VARIADIC_MACROS)
 
+  #define VMD_TEST_THE_DATA 657,1298,the,*,we99,33,++,jjgg,+=
+  
+  int mSize(VMD_DATA_SIZE(VMD_TEST_THE_DATA));
+
+  BOOST_TEST_EQ(mSize,9);
+  
+  mSize VMD_DATA_ELEM(8,VMD_TEST_THE_DATA) 5;
+  
+  BOOST_TEST_EQ(mSize,14);
+  
   int aSize(VMD_DATA_SIZE(23,ll,678,456));
   
   BOOST_TEST_EQ(aSize,4);
+  
+  aSize += mSize VMD_DATA_ELEM(3,VMD_TEST_THE_DATA) VMD_DATA_SIZE(VMD_TEST_THE_DATA);
+  
+  BOOST_TEST_EQ(aSize,130);
+  
+  BOOST_TEST_EQ(BOOST_PP_INC(VMD_DATA_ELEM(5,VMD_TEST_THE_DATA)),34);
   
   int anElement(VMD_DATA_ELEM(2,23,ll,678,456));
   
