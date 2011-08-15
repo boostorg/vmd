3d969872a710d238cd4df36193e94d1764c2a21e
eldiener <eldiener@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-15 17:32:41 +0000

    Updated 'pplib' mode to equal Boost PP macro name.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/variadic_macro_data@73783 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/variadic_macro_data/vmd_to_data_pplib.hpp
+++ boost/variadic_macro_data/vmd_to_data_pplib.hpp
@@ -10,21 +10,10 @@
 #include <boost/preprocessor/seq/enum.hpp>
 #include <boost/preprocessor/tuple/enum.hpp>
 
-#define BOOST_VMD_PP_ARRAY_TO_DATA(array) \
-  BOOST_PP_ARRAY_ENUM(array) \
-/**/
-
-#define BOOST_VMD_PP_LIST_TO_DATA(list) \
-  BOOST_PP_LIST_ENUM(list) \
-/**/
-
-#define BOOST_VMD_PP_SEQ_TO_DATA(seq) \
-  BOOST_PP_SEQ_ENUM(seq) \
-/**/
-
-#define BOOST_VMD_PP_TUPLE_TO_DATA(tuple) \
-  BOOST_PP_TUPLE_ENUM(tuple) \
-/**/
+#define BOOST_VMD_PP_ARRAY_TO_DATA BOOST_PP_ARRAY_ENUM
+#define BOOST_VMD_PP_LIST_TO_DATA BOOST_PP_LIST_ENUM
+#define BOOST_VMD_PP_SEQ_TO_DATA BOOST_PP_SEQ_ENUM
+#define BOOST_VMD_PP_TUPLE_TO_DATA BOOST_PP_TUPLE_ENUM
 
 #endif /* BOOST_VMD_VARIADICS */
 #endif /* VMD_TO_DATA_PPLIB_HPP */
--- boost/variadic_macro_data/vmd_tuple_pplib.hpp
+++ boost/variadic_macro_data/vmd_tuple_pplib.hpp
@@ -12,29 +12,12 @@
 #include <boost/preprocessor/tuple/to_list.hpp>
 #include <boost/preprocessor/tuple/to_seq.hpp>
 
-#define BOOST_VMD_PP_TUPLE_SIZE(tuple) \
-  BOOST_PP_TUPLE_SIZE(tuple) \
-/**/
-
-#define BOOST_VMD_PP_TUPLE_ELEM(n,tuple) \
-  BOOST_PP_TUPLE_ELEM(n,tuple) \
-/**/
-
-#define BOOST_VMD_PP_TUPLE_REM_CTOR(tuple) \
-  BOOST_PP_TUPLE_REM_CTOR(tuple) \
-/**/
-
-#define BOOST_VMD_PP_TUPLE_REVERSE(tuple) \
-  BOOST_PP_TUPLE_REVERSE(tuple) \
-/**/
-
-#define BOOST_VMD_PP_TUPLE_TO_LIST(tuple) \
-  BOOST_PP_TUPLE_TO_LIST(tuple) \
-/**/
-
-#define BOOST_VMD_PP_TUPLE_TO_SEQ(tuple) \
-  BOOST_PP_TUPLE_TO_SEQ(tuple) \
-/**/
+#define BOOST_VMD_PP_TUPLE_SIZE BOOST_PP_TUPLE_SIZE
+#define BOOST_VMD_PP_TUPLE_ELEM BOOST_PP_TUPLE_ELEM
+#define BOOST_VMD_PP_TUPLE_REM_CTOR BOOST_PP_TUPLE_REM_CTOR
+#define BOOST_VMD_PP_TUPLE_REVERSE BOOST_PP_TUPLE_REVERSE
+#define BOOST_VMD_PP_TUPLE_TO_LIST BOOST_PP_TUPLE_TO_LIST
+#define BOOST_VMD_PP_TUPLE_TO_SEQ BOOST_PP_TUPLE_TO_SEQ
 
 #endif /* BOOST_VMD_VARIADICS */
 #endif /* VMD_TUPLE_PPLIB_HPP */
