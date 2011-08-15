35a9d454968d10931df2b55a8dcdb4bbd2a691e8
eldiener <eldiener@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-08-15 02:09:56 +0000

    Updated docs and header arrangement.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/variadic_macro_data@73769 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/variadic_macro_data/detail/vmd_detail_assert_is_tuple.hpp
+++ boost/variadic_macro_data/detail/vmd_detail_assert_is_tuple.hpp
@@ -3,9 +3,45 @@
 
 #include <boost/variadic_macro_data/detail/vmd_detail_setup.hpp>
 
-#if BOOST_VMD_VARIADICS
+#if BOOST_VMD_VARIADICS && BOOST_VMD_ASSERT_DATA
 
-#include <boost/variadic_macro_data/detail/vmd_detail_assert_is_tuple_common.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
+#include <boost/variadic_macro_data/vmd_is_empty.hpp>
 
-#endif /* BOOST_VMD_VARIADICS */
+#if BOOST_VMD_MSVC
+
+#include <boost/preprocessor/control/iif.hpp>
+#include <boost/preprocessor/debug/assert.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
+
+#define VMD_DETAIL_ASSERT_IS_TUPLE_VC_CHECK_RETURN_FAILURE(x) \
+   BOOST_PP_ASSERT \
+     ( \
+     x \
+     ) \
+   BOOST_PP_IIF \
+     ( \
+     x, \
+     BOOST_PP_EMPTY, \
+     VMD_DETAIL_ASSERT_IS_TUPLE_VC_GEN_ERROR_OUTPUT \
+     ) \
+   () \
+/**/
+
+#define VMD_DETAIL_ASSERT_IS_TUPLE_VC_GEN_ERROR_OUTPUT() \
+   typedef char BOOST_VMD_ASSERT_IS_TUPLE_ERROR[-1]; \
+/**/
+
+#define VMD_DETAIL_ASSERT_IS_TUPLE_GEN_ZERO(x) 0
+
+#endif /* BOOST_VMD_MSVC */
+
+#define VMD_DETAIL_ASSERT_IS_TUPLE_IS_NOT_AFTER(x) \
+   BOOST_VMD_IS_EMPTY(VMD_DETAIL_ASSERT_IS_TUPLE_EXPAND_AFTER x BOOST_PP_EMPTY()) \
+/**/
+
+#define VMD_DETAIL_ASSERT_IS_TUPLE_EXPAND_AFTER(...) \
+/**/
+
+#endif /* BOOST_VMD_VARIADICS && BOOST_VMD_ASSERT_DATA */
 #endif /* VMD_DETAIL_ASSERT_IS_TUPLE_HPP */
