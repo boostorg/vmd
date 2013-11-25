e851aa68325fd55e5b7b0d295624313b914ce006
eldiener <xf235zn>
2013-11-25 18:25:04 -0500

    Added new functionality.

---
+++ include/boost/vmd/detail/vmd_detail_paren_or_empty.hpp
@@ -0,0 +1,16 @@
+#if !defined(BOOST_VMD_DETAIL_PAREN_OR_EMPTY_HPP)
+#define BOOST_VMD_DETAIL_PAREN_OR_EMPTY_HPP
+
+#include <boost/preprocessor/logical/bitor.hpp>
+#include <boost/vmd/vmd_is_begin_parens.hpp>
+#include <boost/vmd/vmd_is_empty.hpp>
+
+#define BOOST_VMD_DETAIL_PAREN_OR_EMPTY(param) \
+	BOOST_PP_BITOR \
+		{ \
+		BOOST_VMD_IS_EMPTY(param), \
+		BOOST_VMD_IS_BEGIN_PARENS(param) \
+		} \
+/**/
+
+#endif /* BOOST_VMD_DETAIL_PAREN_OR_EMPTY_HPP */
---
+++ include/boost/vmd/vmd_after_identifier.hpp
@@ -0,0 +1,48 @@
+#if !defined(BOOST_VMD_AFTER_IDENTIFIER_HPP)
+#define BOOST_VMD_AFTER_IDENTIFIER_HPP
+
+#include <boost/vmd/detail/vmd_detail_setup.hpp>
+
+#if BOOST_PP_VARIADICS
+
+#include <boost/preprocessor/control/iif.hpp>
+#include <boost/vmd/detail/vmd_detail_after_identifier.hpp>
+#include <boost/vmd/detail/vmd_detail_paren_or_empty.hpp>
+
+/*
+
+  The succeeding comments in this file are in doxygen format.
+
+*/
+
+/** \file
+*/
+
+/** \brief Expands to the index of a beginning identifier found and the preprocessor tokens after the beginning identifier.
+
+    parameter = a macro parameter.
+    keys  	  = The data may take one of two forms:
+    			it is either a C++ identifier as a unique 'key'
+    			or a Boost PP tuple of the unique 'key's.
+
+    returns   = the result is a tuple of two elements.
+    			If a beginning identifier is not found, the first element 
+    			is 0 and the second element of the tuple is empty.
+   				If a beginning identifier is found, the first element 
+   				is the index, starting with 1, of the identifier 
+   				and the second element is the preprocessor tokens after the identifier.
+    
+*/
+# define BOOST_VMD_AFTER_IDENTIFIER(parameter,keys) \
+    BOOST_PP_IIF \
+      ( \
+      BOOST_VMD_DETAIL_PAREN_OR_EMPTY(param), \
+      BOOST_VMD_DETAIL_AFTER_IDENTIFIER_NOT_FOUND, \
+      BOOST_VMD_DETAIL_AFTER_IDENTIFIER \
+      ) \
+    (parameter,keys) \
+/**/
+
+#endif /* BOOST_PP_VARIADICS */
+
+#endif /* BOOST_VMD_AFTER_IDENTIFIER_HPP */
