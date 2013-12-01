3fa4ba4c5c160d2c7edc23aed37821fc079d61da
eldiener <xf235zn>
2013-12-01 01:09:41 -0500

    Added after_parens test.

--- include/boost/vmd/vmd.hpp
+++ include/boost/vmd/vmd.hpp
@@ -5,6 +5,7 @@
 
 #if BOOST_PP_VARIADICS
 
+#include <boost/vmd/after_parens.hpp>
 #include <boost/vmd/assert_is_array.hpp>
 #include <boost/vmd/assert_is_list.hpp>
 #include <boost/vmd/assert_is_seq.hpp>
