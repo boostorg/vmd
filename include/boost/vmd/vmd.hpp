e182e24ee31b479b842dd9926aa37efeb4ea0f60
Edward Diener <eldiener@tropicsoft.com>
2013-12-14 05:35:44 -0500

    Added tests for is_begin_identifier

--- include/boost/vmd/vmd.hpp
+++ include/boost/vmd/vmd.hpp
@@ -15,6 +15,7 @@
 #include <boost/vmd/begin_identifier.hpp>
 #include <boost/vmd/begin_number.hpp>
 #include <boost/vmd/begin_parens.hpp>
+#include <boost/vmd/is_begin_identifier.hpp>
 #include <boost/vmd/is_begin_parens.hpp>
 #include <boost/vmd/is_empty.hpp>
 #include <boost/vmd/is_identifier.hpp>
