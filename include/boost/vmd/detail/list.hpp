ad554a690bc82b1e0a3286cf77a65845c0d7a948
Edward Diener <eldiener@tropicsoft.com>
2014-03-28 00:49:36 -0400

    List implementation

--- include/boost/vmd/detail/list.hpp
+++ include/boost/vmd/detail/list.hpp
@@ -1,7 +1,46 @@
 #if !defined(BOOST_VMD_DETAIL_LIST_HPP)
 #define BOOST_VMD_DETAIL_LIST_HPP
 
-#define BOOST_VMD_DETAIL_LIST(list) \
+#include <boost/preprocessor/control/iif.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/vmd/is_empty.hpp>
+#include <boost/vmd/detail/empty_result.hpp>
+#include <boost/vmd/is_list.hpp>
+
+#define BOOST_VMD_DETAIL_LIST_SUCCESS(tuple) tuple
+
+#define BOOST_VMD_DETAIL_LIST_CHECK_FOR_LIST(tuple) \
+	BOOST_PP_IIF \
+		( \
+		BOOST_VMD_IS_LIST \
+			( \
+			BOOST_PP_TUPLE_ELEM \
+				( \
+				0, \
+				tuple \
+				) \
+			), \
+		BOOST_VMD_DETAIL_LIST_SUCCESS, \
+		BOOST_VMD_DETAIL_EMPTY_RESULT \
+		) \
+	(tuple)	\
+/**/
+
+#define BOOST_VMD_DETAIL_LIST_CHECK_RETURN(tuple) \
+	BOOST_PP_IIF \
+		( \
+		BOOST_VMD_IS_EMPTY \
+			( \
+			BOOST_PP_TUPLE_ELEM \
+				( \
+				0, \
+				tuple \
+				) \
+			), \
+		BOOST_VMD_DETAIL_EMPTY_RESULT, \
+		BOOST_VMD_DETAIL_LIST_CHECK_FOR_LIST \
+		) \
+	(tuple) \
 /**/
 
 #endif /* BOOST_VMD_DETAIL_LIST_HPP */
--- include/boost/vmd/list.hpp
+++ include/boost/vmd/list.hpp
@@ -5,9 +5,8 @@
 
 #if BOOST_PP_VARIADICS
 
-#include <boost/preprocessor/control/iif.hpp>
-#include <boost/vmd/is_begin_tuple.hpp>
-#include <boost/vmd/detail/tuple.hpp>
+#include <boost/vmd/tuple.hpp>
+#include <boost/vmd/detail/list.hpp>
 
 /*
 
@@ -29,13 +28,10 @@
     
 */
 # define BOOST_VMD_LIST(param) \
-    BOOST_PP_IIF \
-      ( \
-      BOOST_VMD_IS_BEGIN_TUPLE(param), \
-      BOOST_VMD_DETAIL_AFTER_TUPLE, \
-      BOOST_VMD_DETAIL_AFTER_TUPLE_NOT_FOUND \
-      ) \
-    (param) \
+	BOOST_VMD_DETAIL_LIST_CHECK_RETURN \
+		( \
+		BOOST_VMD_TUPLE(param) \
+		) \
 /**/
 
 #endif /* BOOST_PP_VARIADICS */
