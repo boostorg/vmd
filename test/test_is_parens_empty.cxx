45a57a37f38a6ce0b2e9b723ead17663eb993be4
Edward Diener <eldiener@tropicsoft.com>
2014-07-16 20:30:51 -0400

    Add tests for empty parens and an empty list

---
+++ test/test_is_parens_empty.cxx
@@ -0,0 +1,29 @@
+#if defined(BOOST_VMD_TEST_GENERAL_HEADER)
+#include <boost/vmd/vmd.hpp>
+#else
+#include <boost/vmd/is_parens_empty.hpp>
+#endif
+#include <boost/detail/lightweight_test.hpp>
+
+int main()
+  {
+  
+#if BOOST_PP_VARIADICS
+
+  #define DATA () * 4
+  #define DATA2 4 * ()
+  #define DATA3 (X)
+  #define DATA4 ()
+  #define DATA5 (   )
+  
+  BOOST_TEST(!BOOST_VMD_IS_PARENS_EMPTY(DATA));
+  BOOST_TEST(!BOOST_VMD_IS_PARENS_EMPTY(DATA2));
+  BOOST_TEST(!BOOST_VMD_IS_PARENS_EMPTY(DATA3));
+  BOOST_TEST(BOOST_VMD_IS_PARENS_EMPTY(DATA4));
+  BOOST_TEST(BOOST_VMD_IS_PARENS_EMPTY(DATA5));
+  
+#endif
+
+  return boost::report_errors();
+  
+  }
---
+++ test/test_list_empty.cxx
@@ -0,0 +1,32 @@
+#if defined(BOOST_VMD_TEST_GENERAL_HEADER)
+#include <boost/vmd/vmd.hpp>
+#else
+#include <boost/vmd/list.hpp>
+#endif
+#include <boost/detail/lightweight_test.hpp>
+
+int main()
+  {
+  
+#if BOOST_PP_VARIADICS
+
+  #define A_LIST_PLUS (mmf,(34,(^^,(!,BOOST_PP_NIL)))) 456
+  #define PLUS_ALIST yyt (j,(ii%,BOOST_PP_NIL))
+  #define JDATA ggh
+  #define KDATA (a,(b,BOOST_PP_NIL)) name
+  #define A_SEQ ((25,BOOST_PP_NIL))((26,BOOST_PP_NIL))((27,BOOST_PP_NIL))
+  #define EMPTY_LIST BOOST_PP_NIL
+  
+  BOOST_TEST(!BOOST_VMD_IS_EMPTY_LIST(anything));
+  BOOST_TEST(!BOOST_VMD_IS_EMPTY_LIST(A_LIST_PLUS));
+  BOOST_TEST(!BOOST_VMD_IS_EMPTY_LIST(PLUS_ALIST));
+  BOOST_TEST(!BOOST_VMD_IS_EMPTY_LIST(JDATA));
+  BOOST_TEST(!BOOST_VMD_IS_EMPTY_LIST(KDATA));
+  BOOST_TEST(!BOOST_VMD_IS_EMPTY_LIST(A_SEQ));
+  BOOST_TEST(BOOST_VMD_IS_EMPTY_LIST(EMPTY_LIST));
+  
+#endif
+
+  return boost::report_errors();
+  
+  }
