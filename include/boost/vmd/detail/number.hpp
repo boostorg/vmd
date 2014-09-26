2bf4e7d0d3a63b06008630cd63ce1be6eddf4c90
Edward Diener <eldiener@tropicsoft.com>
2014-09-26 19:10:56 -0400

    Fixed some sequence errors and changed number to be a subset of identifiers with its own subset registration.

--- include/boost/vmd/detail/number.hpp
+++ include/boost/vmd/detail/number.hpp
@@ -2,25 +2,45 @@
 #define BOOST_VMD_DETAIL_NUMBER_HPP
 
 #include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/comparison/equal.hpp>
 #include <boost/preprocessor/control/iif.hpp>
-#include <boost/preprocessor/tuple/enum.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/tuple/replace.hpp>
+#include <boost/preprocessor/tuple/size.hpp>
 #include <boost/vmd/identity.hpp>
 #include <boost/vmd/is_begin_tuple.hpp>
 #include <boost/vmd/tuple.hpp>
+#include <boost/vmd/types.hpp>
 #include <boost/vmd/detail/empty_result.hpp>
 #include <boost/vmd/detail/idprefix.hpp>
 #include <boost/vmd/detail/is_number_concatenate.hpp>
 
+#define BOOST_VMD_DETAIL_NUMBER_REGISTRATION_PREFIX BOOST_VMD_SUBSET_REGISTER_
+
 #define BOOST_VMD_DETAIL_IS_NUMBER_CONCATENATE(parameter) \
 	BOOST_PP_CAT \
 		( \
-		BOOST_VMD_DETAIL_IDENTIFIER_REGISTRATION_PREFIX, \
+		BOOST_VMD_DETAIL_NUMBER_REGISTRATION_PREFIX, \
 		parameter \
 		) \
 /**/
 
+#define BOOST_VMD_DETAIL_IS_NUMBER_CONC_ICTUPLE(vpar) \
+	BOOST_PP_IIF \
+		( \
+		BOOST_VMD_IS_TUPLE(vpar), \
+		BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST_TUPLE, \
+		BOOST_VMD_IDENTITY(0) \
+		) \
+	(vpar) \
+/**/
+
+#define BOOST_VMD_DETAIL_IS_NUMBER_CONC_CTUPLE(vpar) \
+	BOOST_VMD_IDENTITY_RESULT(BOOST_VMD_DETAIL_IS_NUMBER_CONC_ICTUPLE(vpar)) \
+/**/
+
 #define BOOST_VMD_DETAIL_IS_NUMBER_CONC(parameter) \
-	BOOST_VMD_IS_TUPLE \
+	BOOST_VMD_DETAIL_IS_NUMBER_CONC_CTUPLE \
 		( \
 		BOOST_VMD_DETAIL_IS_NUMBER_CONCATENATE(parameter) \
 		) \
@@ -40,45 +60,91 @@
 	BOOST_VMD_IDENTITY_RESULT(BOOST_VMD_DETAIL_IS_NUMBER_IRESULT(parameter)) \
 /**/
 
-#define BOOST_VMD_DETAIL_NUMBER_GET_NUMBER(vseq) \
-	BOOST_PP_TUPLE_ENUM \
+#define BOOST_VMD_DETAIL_NUMBER_GET_TP(tuple) \
+	BOOST_PP_TUPLE_REPLACE \
 		( \
-		BOOST_VMD_BEGIN_TUPLE \
+		tuple, \
+		0, \
+		BOOST_PP_TUPLE_ELEM \
 			( \
-			BOOST_VMD_DETAIL_IS_NUMBER_CONCATENATE(vseq) \
+			1, \
+			BOOST_PP_TUPLE_ELEM(0,tuple) \
 			) \
 		) \
 /**/
 
-#define BOOST_VMD_DETAIL_NUMBER_GET_REST(vseq) \
-	BOOST_VMD_AFTER_TUPLE \
+#define BOOST_VMD_DETAIL_NUMBER_GET(tvseq) \
+	BOOST_VMD_DETAIL_NUMBER_GET_TP(BOOST_VMD_TUPLE(tvseq)) \
+/**/
+
+#define BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST_TUPLE_TYPE(tuple) \
+	BOOST_PP_EQUAL \
 		( \
-		BOOST_VMD_DETAIL_IS_NUMBER_CONCATENATE(vseq) \
+		BOOST_PP_TUPLE_ELEM(0,tuple), \
+		BOOST_VMD_TYPE_NUMBER \
 		) \
 /**/
 
-#define BOOST_VMD_DETAIL_NUMBER_GET(vseq) \
-	( \
-	BOOST_VMD_DETAIL_NUMBER_GET_NUMBER(vseq), \
-	BOOST_VMD_DETAIL_NUMBER_GET_REST(vseq) \
-	) \
-/**/
-
-#define BOOST_VMD_DETAIL_NUMBER_NEXT_PEN(vseq) \
-	BOOST_VMD_IS_BEGIN_TUPLE \
-		( \
-		BOOST_VMD_DETAIL_IS_NUMBER_CONCATENATE(vseq) \
-		) \
-/**/
-
-#define BOOST_VMD_DETAIL_NUMBER_PROCESS(vseq) \
+#define BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST_ITUPLE(tuple) \
 	BOOST_PP_IIF \
 		( \
-		BOOST_VMD_DETAIL_NUMBER_NEXT_PEN(vseq), \
+		BOOST_PP_EQUAL \
+			( \
+			BOOST_PP_TUPLE_SIZE(tuple), \
+			2 \
+			), \
+		BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST_TUPLE_TYPE, \
+		BOOST_VMD_IDENTITY(0) \
+		) \
+	(tuple) \
+/**/
+
+#define BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST_TUPLE(tuple) \
+	BOOST_VMD_IDENTITY_RESULT \
+		( \
+		BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST_ITUPLE(tuple) \
+		) \
+/**/
+
+#define BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST(tvseq) \
+	BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST_TUPLE \
+		( \
+		BOOST_VMD_BEGIN_TUPLE(tvseq) \
+		) \
+/**/
+
+#define BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_IVSEQ(tvseq) \
+	BOOST_PP_IIF \
+		( \
+		BOOST_VMD_IS_BEGIN_TUPLE(tvseq), \
+		BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_TEST, \
+		BOOST_VMD_IDENTITY(0) \
+		) \
+	(tvseq) \
+/**/
+
+#define BOOST_VMD_DETAIL_NUMBER_NEXT_PEN(tvseq) \
+	BOOST_VMD_IDENTITY_RESULT \
+		( \
+		BOOST_VMD_DETAIL_NUMBER_NEXT_PEN_IVSEQ(tvseq) \
+		) \
+/**/
+
+#define BOOST_VMD_DETAIL_NUMBER_PROCESS_VSEQ(tvseq) \
+	BOOST_PP_IIF \
+		( \
+		BOOST_VMD_DETAIL_NUMBER_NEXT_PEN(tvseq), \
 		BOOST_VMD_DETAIL_NUMBER_GET, \
 		BOOST_VMD_DETAIL_EMPTY_RESULT \
 		) \
-	(vseq) \
+	(tvseq) \
+/**/
+
+#define BOOST_VMD_DETAIL_NUMBER_PROCESS(vseq) \
+	BOOST_VMD_DETAIL_NUMBER_PROCESS_VSEQ \
+		( \
+		BOOST_VMD_DETAIL_IS_NUMBER_CONCATENATE(vseq) \
+		) \
 /**/
 
 #define BOOST_VMD_DETAIL_NUMBER(vseq) \
