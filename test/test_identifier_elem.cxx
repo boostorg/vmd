d96b3aa8bdf6d6d8039ebc54874be16eb6a009aa
Edward Diener <eldiener@tropicsoft.com>
2014-11-06 05:21:33 -0500

    Added element tests.

---
+++ test/test_identifier_elem.cxx
@@ -0,0 +1,18 @@
+#if defined(BOOST_VMD_TEST_GENERAL_HEADER)
+#include <boost/vmd/vmd.hpp>
+#else
+#include <boost/vmd/identifier_elem.hpp>
+#include <boost/vmd/is_empty.hpp>
+#endif
+#include <boost/detail/lightweight_test.hpp>
+
+int main()
+  {
+  
+#if BOOST_PP_VARIADICS
+
+#endif
+
+  return boost::report_errors();
+  
+  }
