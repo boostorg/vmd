be65d508218cf138694d8d4ed1c487467137e7f1
Edward Diener <eldiener@tropicsoft.com>
2014-12-15 14:17:46 -0500

    Use equal and not_equal in generic.

---
+++ include/boost/vmd/generic/not_equal.hpp
@@ -0,0 +1,20 @@
+#if !defined(BOOST_VMD_NOT_EQUAL_HPP)
+#define BOOST_VMD_NOT_EQUAL_HPP
+
+#include <boost/vmd/detail/setup.hpp>
+
+#if BOOST_PP_VARIADICS
+
+#include <boost/preprocessor/logical/compl.hpp>
+#include <boost/vmd/generic/equal.hpp>
+
+#define BOOST_VMD_NOT_EQUAL(vseq1,vseq2) \
+	BOOST_PP_COMPL(BOOST_VMD_EQUAL(vseq1,vseq2)) \
+/**/
+
+#define BOOST_VMD_NOT_EQUAL_D(d,vseq1,vseq2) \
+	BOOST_PP_COMPL(BOOST_VMD_EQUAL_D(d,vseq1,vseq2)) \
+/**/
+
+#endif /* BOOST_PP_VARIADICS */
+#endif /* BOOST_VMD_NOT_EQUAL_HPP */
