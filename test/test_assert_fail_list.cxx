b2495e0cbc84f5bc5c461db8d45d998c16de7668
Edward Diener <eldiener@tropicsoft.com>
2015-01-25 22:11:05 -0500

    Update tests to use elem

--- test/test_assert_fail_list.cxx
+++ test/test_assert_fail_list.cxx
@@ -2,7 +2,7 @@
 #include <boost/vmd/vmd.hpp>
 #else
 #include <boost/vmd/assert.hpp>
-#include <boost/vmd/list/list_elem.hpp>
+#include <boost/vmd/elem.hpp>
 #endif
 #include <boost/detail/lightweight_test.hpp>
 #include <boost/preprocessor/comparison/equal.hpp>
@@ -15,7 +15,7 @@ int main()
 
   #define A_LIST_PLUS (mmf,(34,(^^,(!,BOOST_PP_NIL)))) 56
   
-  BOOST_VMD_ASSERT(BOOST_PP_EQUAL(BOOST_PP_TUPLE_ELEM(1,BOOST_VMD_LIST_ELEM(0,A_LIST_PLUS,BOOST_VMD_RETURN_AFTER)),36),BOOST_VMD_TEST_FAIL_LIST)
+  BOOST_VMD_ASSERT(BOOST_PP_EQUAL(BOOST_PP_TUPLE_ELEM(1,BOOST_VMD_ELEM(0,A_LIST_PLUS,BOOST_VMD_RETURN_AFTER,BOOST_VMD_TYPE_LIST)),36),BOOST_VMD_TEST_FAIL_LIST)
   
 #else
   
