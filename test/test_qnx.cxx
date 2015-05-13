7388b219316c94bfd5caa698fc6276d9f64e79e4
Edward Diener <eldiener@tropicsoft.com>
2015-05-13 00:57:30 -0400

    Corrected predef notation in test

--- test/test_qnx.cxx
+++ test/test_qnx.cxx
@@ -6,19 +6,19 @@ int main()
   {
   
 #    if BOOST_OS_QNX
-#      if BOOST_COMP_GNUC >= 4.3
+#      if BOOST_COMP_GNUC >= BOOST_VERSION_NUMBER(4,3,0)
 #        if !defined __cplusplus && __STDC_VERSION__ < 199901L
-BOOST_ERROR("Gcc >= 4.3: __STDC_VERSION__ is less than 199901L.");
+BOOST_ERROR("QNX operating system and Gcc >= 4.3: __STDC_VERSION__ is less than 199901L.");
 #        elif defined __cplusplus && __cplusplus < 201103L
-BOOST_ERROR("Gcc >= 4.3: __cplusplus is less than 201103L.");
+BOOST_ERROR("QNX operating system and Gcc >= 4.3: __cplusplus is less than 201103L.");
 #        else
 BOOST_ERROR("QNX operating system and Gcc >= 4.3.");
 #        endif
 #      else
 #        if !defined __cplusplus && __STDC_VERSION__ < 199901L
-BOOST_ERROR("__STDC_VERSION__ is less than 199901L.");
+BOOST_ERROR("QNX operating system and __STDC_VERSION__ is less than 199901L.");
 #        elif defined __cplusplus && __cplusplus < 201103L
-BOOST_ERROR("__cplusplus is less than 201103L.");
+BOOST_ERROR("QNX operating system and __cplusplus is less than 201103L.");
 #        else
 BOOST_ERROR("QNX operating system.");
 #        endif
