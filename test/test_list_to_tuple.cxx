daa7b063e6b091a1242e873e2c97dc864c69589c
Edward Diener <eld@tropicsoft.com>
2015-10-17 14:53:19 -0400

    Tests for array, list, seq, tuple functionality with empty seqs and tuples

---
+++ test/test_list_to_tuple.cxx
@@ -0,0 +1,27 @@
+
+//  (C) Copyright Edward Diener 2015
+//  Use, modification and distribution are subject to the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt).
+
+#if defined(BOOST_VMD_TEST_GENERAL_HEADER)
+#include <boost/vmd/vmd.hpp>
+#else
+#include <boost/vmd/list/to_tuple.hpp>
+#endif
+#include <boost/detail/lightweight_test.hpp>
+
+int main()
+  {
+  
+#if BOOST_PP_VARIADICS
+
+#else
+
+BOOST_ERROR("No variadic macro support");
+  
+#endif
+
+  return boost::report_errors();
+  
+  }
